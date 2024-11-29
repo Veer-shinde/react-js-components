f8d9ed44e7e8e88debf32eb3cd628b0a1e457f48
Veer Shinde <shindeveer03@gmail.com>
2024-11-29 13:06:15 +0530

    add props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import { useState } from "react";
+
 import Gallery from "./Gallery";
 import Profile from "./Profile";
+import './App.css';
+import Card from "./components/Card";
+import Buttons from "./components/Buttons";
+
+
 function App() {
+const [count, setCount] = useState(0);
+function handleClick() {
+  setCount (count+1);
+}
+
   return (
     <>
-    <Gallery/>
+    <Buttons handleClick={handleClick} text="click Me">
+    <h1> {count} </h1>
+    </Buttons>
+  
+
+
+    {/* <Gallery/>
     <Profile/>
-    
+    <Card name="veer shinde"> 
+    <h1> lorem test this dummy </h1>
+    <p> react components is javascript reguler funtion</p>
+    <p> components is reusable and component start with capital letter</p>
+    </Card> */}
+
     </>
   );
 }
--- src/Profile.js
+++ src/Profile.js
@@ -3,12 +3,14 @@ import React from 'react'
 export default function Profile () {
   return (
     <>
+    <div className='container'>
     {/* Hello world */}
     <div className="awesome" style={{ border: "1px solid red" }}>
       <label htmlFor="name">Enter your name: </label>
       <input type="text" id="name" />
     </div>
     <p>Enter your HTML here</p>
+    </div>
   </>
   
   );
